901a9b5e34bf8bb99f22b5ece72ec8e305f9b956
leawn <leonrubner2809@gmail.com>
2024-10-26 04:32:28 +0200

    Change the way of returning client

--- openai/integrations_python_openai/utils/client.py
+++ openai/integrations_python_openai/utils/client.py
@@ -1,11 +1,7 @@
 import os
 from openai import OpenAI
 
-openai_instance: OpenAI | None = None
-
 def openai_client(api_key: str | None = os.getenv("OPENAI_API_KEY")) -> OpenAI:
     if not api_key:
         raise ValueError("API key is required to create OpenAI client")
-    if openai_instance is None:
-        openai_instance = OpenAI(api_key=api_key)
-    return openai_instance
+    return OpenAI(api_key=api_key)
