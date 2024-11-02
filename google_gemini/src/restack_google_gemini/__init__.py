ae7e531e74344585e49ce32beeb7c02bb006ffe4
leawn <leonrubner2809@gmail.com>
2024-11-02 15:08:02 +0100

    Add Google Gemini Chat functionality

--- google_gemini/src/restack_google_gemini/__init__.py
+++ google_gemini/src/restack_google_gemini/__init__.py
@@ -2,6 +2,9 @@
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 from .functions.generate_content import gemini_generate_content, GeminiGenerateContentInput
+from .functions.chat.start_chat import gemini_start_chat, GeminiStartChatInput
+from .functions.chat.send_message import gemini_send_message, GeminiSendMessageInput
+from .functions.chat.get_history import gemini_get_history, GeminiGetHistoryInput
 from .service import GeminiServiceOptions, GeminiServiceInput, gemini_service
 
 __all__ = [
@@ -9,5 +12,11 @@ __all__ = [
     'GeminiGenerateContentInput',
     'GeminiServiceOptions',
     'GeminiServiceInput',
-    'gemini_service'
+    'gemini_service',
+    'gemini_start_chat',
+    'GeminiStartChatInput',
+    'gemini_send_message',
+    'GeminiSendMessageInput',
+    'gemini_get_history',
+    'GeminiGetHistoryInput'
 ]
