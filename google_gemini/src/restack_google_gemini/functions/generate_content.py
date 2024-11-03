15819bfa6caffe8f6c74b0a1257d561373d536fe
imjwang <ngjaywa@gmail.com>
2024-11-02 21:41:56 -0700

    gemini system message

--- google_gemini/src/restack_google_gemini/functions/generate_content.py
+++ google_gemini/src/restack_google_gemini/functions/generate_content.py
@@ -6,14 +6,15 @@ class GeminiGenerateContentInput(BaseModel):
     model: str | None = None
     api_key: str = ""
     generation_config: dict | None = None
-
+    system_instruction: str | None = None
+    
 def gemini_generate_content(input) :
     if not input.api_key:
         raise ValueError("api_key is required")
     
     genai.configure(api_key=input.api_key)
 
-    model = genai.GenerativeModel(model_name=input.model)
+    model = genai.GenerativeModel(model_name=input.model, system_instruction=input.system_instruction)
 
     response = model.generate_content(input.user_content, generation_config=genai.GenerationConfig(**input.generation_config))
 
